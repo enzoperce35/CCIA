0bab1ac5d3c7994f8f9303ad384a914a8c1c586f
enzoperce35 <enzoperce35@gmail.com>
2022-04-23 14:29:58 +0800

    possible deployment fix

--- app/helpers/coins_helper.rb
+++ app/helpers/coins_helper.rb
@@ -1,5 +1,3 @@
-require_relative './extra_values.rb'
-
 module CoinsHelper
 
   def switch_status( coin )
@@ -64,6 +62,30 @@ module CoinsHelper
   def current_price_of(coin)
     coin['current_price']
   end
+
+  def insert_pair(h, key, pair, proximity=:before)
+    h.to_a.insert(h.keys.index(key) + (proximity==:after ? 1 : 0), pair.first).to_h
+  end
+  
+  def insert_user_gain_of(coin)
+    coin.store( 'user_price_gain',
+                 percentage_between( current_price_of(coin),
+                 entry_price_of(coin) ) - 100 )
+  end
+  
+  def get_difference( high, low, current )
+    range = high - low
+  
+    current -= low
+  
+    percentage_between(current, range)
+  end
+  
+  def insert_current_vs_24h_prices_of(coin)
+    difference = get_difference( high_24h(coin), low_24h(coin), current_price_of(coin) )
+  
+    coin.store( "vs_24h", difference )
+  end  
 
   def insert_values_from(coin)
     coin = market(coin)
