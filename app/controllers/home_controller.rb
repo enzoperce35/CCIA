05c2db58e1aa5a4157e0439f9cdcf4edfbc65591
enzoperce35 <enzoperce35@gmail.com>
2022-04-28 07:25:26 +0800

    fix test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @selected_coin = params[:selected]
     @observe = params[:observe]
     @coins = helpers.insert_extra_values_from( Coin.all )
   end
